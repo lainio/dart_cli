b9cdf6c87cb6706ae145fe5b4efdab0ad4fe89db
Harri Lainio <harri.lainio@iki.fi>
2023-11-01 18:59:21 +0200

    mid point

--- bin/cli.dart
+++ bin/cli.dart
@@ -1,4 +1,5 @@
 import 'package:cryptography/cryptography.dart';
+import 'package:dart_wot/dart_wot.dart';
 
 Future<void> main() async {
   final algorithm = AesGcm.with256bits();
@@ -19,7 +20,17 @@ Future<void> main() async {
   print('Ciphertext: ${secretBox.cipherText}');
   print('MAC: ${secretBox.mac}');
 
+  final coseKey = CoseKey(keyType: KeyType.ec2, keyId: [1, 2, 3], algorithm: Algorithm.es256);
+  print(coseKey.toString());
+  final cosePubKey = PlainCoseKey(coseKey);
+  final pkStr = cosePubKey.toString();
+  print(pkStr);
+  
+  print('=============');
   await main2();
+  
+  print('=============');
+  await main3();
 }
 
 Future<void> main2() async {
@@ -27,6 +38,9 @@ Future<void> main2() async {
 
   // Generate a key pair
   final keyPair = await algorithm.newKeyPair();
+  final e = await keyPair.extractPrivateKeyBytes();
+  print('privatekeybytes');
+  print(e.toString());
 
   // Sign a message
   final message = <int>[1,2,3];
@@ -45,4 +59,29 @@ Future<void> main2() async {
     signature: signature,
   );
   print('signature: $isSignatureCorrect');
+}
+
+Future<void> main3() async {
+  // In this example, we use ECDSA-P256-SHA256
+  final algorithm = Ecdsa.p256(Sha256());
+
+  print(algorithm.keyPairType);
+  // Generate a random key pair
+  final secretKey = await algorithm.newKeyPair();
+  //final secretKey = await algorithm.newSecretKey();
+  //final publicKey = await algorithm.publicKey(secretKey);
+
+  // Sign a message
+  final message = <int>[1,2,3];
+  final signature = await algorithm.sign(
+    [1,2,3],
+    keyPair: secretKey,
+  );
+
+  // Anyone can verify the signature
+  final isVerified = await algorithm.verify(
+    message,
+    signature: signature,
+  );
+  print('signature: $isVerified');
 }
