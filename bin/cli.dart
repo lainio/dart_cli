a352faf78225556a36b82ebfdde37608498309f0
Harri Lainio <harri.lainio@iki.fi>
2023-11-02 20:12:31 +0200

    one new step

--- bin/cli.dart
+++ bin/cli.dart
@@ -1,5 +1,68 @@
 import 'package:cryptography/cryptography.dart';
-import 'package:dart_wot/dart_wot.dart';
+//import 'package:dart_wot/dart_wot.dart';
+
+import 'dart:convert';
+import 'dart:math';
+import 'dart:typed_data';
+
+import 'package:pointycastle/asn1.dart';
+import 'package:pointycastle/export.dart';
+
+import 'package:basic_utils/basic_utils.dart';
+
+Future<void> main3() async {
+  // the private key
+  ECPrivateKey? privateKey;
+  
+  final akp = CryptoUtils.generateEcKeyPair();
+  final pubKey = akp.publicKey as ECPublicKey;
+  final x = pubKey.Q!.x;
+  print(x);
+  privateKey = akp.privateKey as ECPrivateKey;
+
+  // some bytes to sign
+  //final bytes = Uint8List(0);
+
+  final url = 'My URL string...';
+  //Convert the URL string to Uint8List
+  final bytes = utf8.encode(url);
+
+  // a suitable random number generator - create it just once and reuse
+  final rand = Random.secure();
+  final fortunaPrng = FortunaRandom()
+    ..seed(KeyParameter(Uint8List.fromList(List<int>.generate(
+      32,
+      (_) => rand.nextInt(256),
+    ))));
+
+  // the ECDSA signer using SHA-256
+  final signer = ECDSASigner(SHA256Digest())
+    ..init(
+      true,
+      ParametersWithRandom(
+        PrivateKeyParameter(privateKey),
+        fortunaPrng,
+      ),
+    );
+
+  // sign the bytes
+  final ecSignature = signer.generateSignature(bytes) as ECSignature;
+
+  // encode the two signature values in a common format
+  // hopefully this is what the server expects
+  final encoded = ASN1Sequence(elements: [
+    ASN1Integer(ecSignature.r),
+    ASN1Integer(ecSignature.s),
+  ]).encode();
+
+  // and finally base 64 encode it
+  final signature = base64UrlEncode(encoded);
+  print(signature);
+
+  ECSignature sig = CryptoUtils.ecSign(privateKey, bytes, algorithmName: 'SHA-256/ECDSA');
+  final signature2 = CryptoUtils.ecSignatureToBase64(sig);
+  print(signature2);
+}
 
 Future<void> main() async {
   final algorithm = AesGcm.with256bits();
@@ -20,16 +83,11 @@ Future<void> main() async {
   print('Ciphertext: ${secretBox.cipherText}');
   print('MAC: ${secretBox.mac}');
 
-  final coseKey = CoseKey(keyType: KeyType.ec2, keyId: [1, 2, 3], algorithm: Algorithm.es256);
-  print(coseKey.toString());
-  final cosePubKey = PlainCoseKey(coseKey);
-  final pkStr = cosePubKey.toString();
-  print(pkStr);
-  
   print('=============');
   await main2();
   
   print('=============');
+  await main3();
 }
 
 Future<void> main2() async {
