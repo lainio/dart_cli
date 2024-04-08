fff6dbac16b0f8ac5ff9a4d50f34d5ff1dd4bbe4
Harri Lainio <harri.lainio@iki.fi>
2024-04-08 18:48:11 +0300

    starting to merge configs and bases

--- packages/authn/lib/src/authn_base.dart
+++ packages/authn/lib/src/authn_base.dart
@@ -1,4 +1,9 @@
+import 'command.dart';
 import 'auth_client.dart';
+
+class AuthnCommand extends Command {
+  AuthnCommand(super.addr, super.port);
+}
 
 Future<String> authnCmd(String cmd, name, xorKey) async {
   return exec(cmd, name, xorKey);
