e3cb369f49f20bffc7180e32d3fb2342eebaf648
Harri Lainio <harri.lainio@iki.fi>
2024-04-10 10:48:22 +0300

    support code defaults and cfg files, usage update

--- bin/cli.dart
+++ bin/cli.dart
@@ -6,8 +6,9 @@ const neededArgsSizeForCloud = 4;
 Future<void> main(List<String> args) async {
   if (args.length < neededArgsSizeForCloud - 1 ||
       args.length > neededArgsSizeForCloud) {
-    print('Usage: <login/register> <name> <keyID> [op]\n');
-    print('       op -> contacts cloud installation');
+    print('Usage: <login/register> <name> <keyID> [your-cfg-file.yaml]\n');
+    print('       your-cfg-file.yaml: enter your configurations in YAML,');
+    print('                           or defauls are used\n');
     return;
   }
 
@@ -16,9 +17,9 @@ Future<void> main(List<String> args) async {
   final pin = args[2];
   print('cmd: $cmd, name: $name, keyID: $pin');
   if (args.length == neededArgsSizeForCloud) {
-    setupFromYAML('cfg-op.yaml');
+    setupFromYAML(args[3]);
   } else {
-    setupFromYAML('cfg.yaml');
+    setupDefaults();
   }
   final jwt = await authnCmd(cmd, name, pin);
   print(jwt);
